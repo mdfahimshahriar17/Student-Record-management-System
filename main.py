aaf754c201e69fb84d1ff675048f83c99bd8a71f
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-01-28 18:18:11 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print("========== MENU ==========")
+    print("1. Add Student")
+    print("2. View Student")
+    print("3. Search Student")
+    print("4. Remove Student")
+    print("5. Exit")
+
+
+if __name__=="__main__":
+    main()
